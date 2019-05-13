53616655845c02ea7087ee17fb3926f528469157
Shendidy <sherif@shendidy.com>
2019-05-13 14:56:26 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# Timer for ruby code
+
+To use this class, require it into your project, then create a new object of this class.
+
+To get the duration your code took to run, just pass your code as a yield into your new object's duration method.
+# ****************
+ex: you want to measure the duration of iterating from 0 to 1000000
+
+timer = Timer.new
+
+iteration_timer = timer.duration { 
+
+    i = 0
+
+    while i < 1000000
+
+        i += 1
+
+    end
+
+}
+
+Now iteration_timer = some seconds
--- timer.rb
+++ timer.rb
@@ -1,16 +1,3 @@
-# To use this class, require it into your project, then create a new object of this class
-# to get the duration your code took to run, just pass your code as a yield into your new object's duration method.
-# ****************
-# ex: you want to measure the duration of iterating from 0 to 1000000
-# timer = Timer.new
-# iteration_timer = timer.duration { 
-#   i = 0
-#   while i < 1000000
-#     i += 1
-#   end
-#  }
-# now iteration_timer = some seconds
-
 class Timer
   def duration
     start_time = Time.now

35da8487d63e220fb319040c0c487cafa1f72e71
Shendidy <sherif@shendidy.com>
2019-05-16 05:34:29 +0100

    updated timer file

--- timer.rb
+++ timer.rb
@@ -4,7 +4,16 @@ class Timer
     # puts "starting time: #{start_time}" 
     yield
     end_time = Time.now
-    # puts "End time: #{end_time}"
+    # puts "Ending time: #{end_time}"
     duration = end_time - start_time
   end
 end
+
+# timer = Timer.new
+# puts timer.duration{
+#   i = 0
+#   while i < 10000000
+#     i
+#     i+= 1
+#   end
+# }

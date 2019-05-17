642f2b65836c212caf29eed9991791cc21929bfc
Shendidy <sherif@shendidy.com>
2019-05-17 11:35:39 +0100

    reverse using push

--- lib/reverse.rb
+++ lib/reverse.rb
@@ -2,4 +2,14 @@ def reverse_unshift(arr)
   new_array = [] # O(1)
   arr.each{|e| new_array.unshift(e)} # O(n)
   new_array # O(1)
-end
+end # final type of complexity is "O(n)"
+
+def reverse_push(arr)
+  new_array = [] # O(1)
+  i = arr.length # O(1)
+  while i > 0 # O(1)
+    new_array << arr[i - 1] # O(n)
+    i -= 1 # O(1)
+  end
+  new_array # O(1)
+end # final type of complexity is "O(n)"
--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -15,4 +15,18 @@ describe 'reverse' do
       expect(reverse_unshift([1, 2, 3, 4, 5, 6, 7])).to eq([7, 6, 5, 4, 3, 2 ,1])
     end
   end
+
+  context '2- push' do
+    it 'a- should return array of reversed elements' do
+      expect(reverse_push([1, 2, 3])).to eq([3, 2, 1])
+    end
+
+    it 'b- should return array of reversed elements' do
+      expect(reverse_push(["a", "b", "c"])).to eq(["c", "b", "a"])
+    end
+
+    it 'c- should return longer arrays of reversed elements' do
+      expect(reverse_push([1, 2, 3, 4, 5, 6, 7])).to eq([7, 6, 5, 4, 3, 2 ,1])
+    end
+  end
 end

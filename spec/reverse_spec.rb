95d40fe0b38f0c2c1c9334ce83588df94c5995fd
Shendidy <sherif@shendidy.com>
2019-05-17 13:38:16 +0100

    reverse by swapping contents of location within the array

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -29,4 +29,18 @@ describe 'reverse' do
       expect(reverse_push([1, 2, 3, 4, 5, 6, 7])).to eq([7, 6, 5, 4, 3, 2 ,1])
     end
   end
+
+  context '3- swap' do
+    it 'a- should return array of reversed elements' do
+      expect(reverse_swap([1, 2, 3])).to eq([3, 2, 1])
+    end
+
+    # it 'b- should return array of reversed elements' do
+    #   expect(reverse_swap(["a", "b", "c"])).to eq(["c", "b", "a"])
+    # end
+
+    # it 'c- should return longer arrays of reversed elements' do
+    #   expect(reverse_swap([1, 2, 3, 4, 5, 6, 7])).to eq([7, 6, 5, 4, 3, 2 ,1])
+    # end
+  end
 end
